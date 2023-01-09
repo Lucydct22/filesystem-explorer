1a48d6283e5b693ab7dac05d8d2d83d5117475d6
lucydct <tenalucia0@gmail.com>
2023-01-09 17:49:00 +0100

    testupdate2

---
+++ root/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <nav class="navbar">
+            <img src="" alt="">
+            <input type="search">
+        </nav>
+    </header>
+
+    <div class="options">
+        <a href="">Edit</a>
+        <a href="">Upload</a>
+        <a href="">Create</a>
+        <a href="">Delete</a>
+
+    </div>
+
+</body>
+
+</html>
