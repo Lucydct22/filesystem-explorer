937737b6e3fb24c86dc0e231e0ffef516b32fc46
lucydct <tenalucia0@gmail.com>
2023-01-10 10:01:32 +0100

    update4

--- root/index.php
+++ root/index.php
@@ -6,13 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="script.js" defer></script>
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link href="bootstrap.min.css" rel="stylesheet" />
 </head>
 
 <body>
 
     <header>
         <nav class="navbar">
-            <img src="" alt="">
+            <img src="assets/logo.png" alt="logo" class="logo">
             <input type="search">
         </nav>
     </header>
@@ -24,7 +29,45 @@
         <a href="">Delete</a>
 
     </div>
+    <div class="main-container">
+        <div class="files-container">
+            <p>Root</p>
+            <ul>
+                <li>doc</li>
+                <li>csv</li>
+                <li>jpg</li>
+                <li>png</li>
+                <li>txt</li>
+                <li>ppt</li>
+                <li>odt</li>
+                <li>pdf</li>
+                <li>zip</li>
+                <li>rar</li>
+                <li>exe</li>
+                <li>svg</li>
+                <li>mp3</li>
+                <li>mp4</li>
+            </ul>
+        </div>
 
+        <div class="content-container">
+
+        </div>
+
+        <div class="edition-container">
+            <ul>
+                <li>Creation date</li>
+                <li>Last modified date</li>
+                <li>Extension</li>
+                <li>Size</li>
+            </ul>
+        </div>
+        <div class="erase-container">
+            <ul>
+                <li>Trash</li>
+            </ul>
+        </div>
+    </div>
 </body>
 
 </html>
