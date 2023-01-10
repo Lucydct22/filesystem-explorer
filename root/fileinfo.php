0a83ef8a226383bc4c56ec9e7b83faa06683ab40
lucydct <tenalucia0@gmail.com>
2023-01-10 16:06:13 +0100

    update8

--- root/fileinfo.php
+++ root/fileinfo.php
@@ -1,21 +1,22 @@
 <?php
 
-print_r($_FILES);
 
-$name = $_FILES['uploadBtn']['name'];
-$saved = $_FILES['uploadBtn']['tmp_name'];
+$archive = $_FILES["uploadedFile"]['name'];
+$saved = $_FILES["uploadedFile"]['tmp_name'];
+$btn = $_POST["uploadBtn"];
+
 
 if (!file_exists("uploadBtn")) {
     mkdir("uploadBtn", 0777, true);
     if (file_exists("uploadBtn")) {
-        if (move_uploaded_file($saved, "uploadBtn/" . $name)) {
+        if (move_uploaded_file($saved, "uploadBtn/" . $archive)) {
             echo "Archive saved succesfully";
         } else {
             echo "Archive could not be saved";
         }
     }
 } else {
-    if (move_uploaded_file($saved, "uploadBtn/" . $name)) {
+    if (move_uploaded_file($saved, "uploadBtn/" . $archive)) {
         echo "Archive saved succesfully";
     } else {
         echo "Archive could not be saved";
