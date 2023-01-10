447d2bb1e390664cb5b40aff187bea38a71fb365
Berta Garcia <bertagnavarro93@gmail.com>
2023-01-10 17:03:54 +0100

    commit

--- root/fileinfo.php
+++ root/fileinfo.php
@@ -1,13 +1,12 @@
 <?php
 
-
 $archive = $_FILES["uploadedFile"]['name'];
 $saved = $_FILES["uploadedFile"]['tmp_name'];
 $btn = $_POST["uploadBtn"];
 
 
 if (!file_exists("uploadBtn")) {
-    mkdir("uploadBtn", 0777, true);
+    mkdir("uploadBtn", 0777, false);
     if (file_exists("uploadBtn")) {
         if (move_uploaded_file($saved, "uploadBtn/" . $archive)) {
             echo "Archive saved succesfully";
@@ -21,4 +20,6 @@ if (!file_exists("uploadBtn")) {
     } else {
         echo "Archive could not be saved";
     }
-};
+    ;
+}
+?>
