27b234ed8466ea409f438c8cc4aa12c9b09df174
lucydct <tenalucia0@gmail.com>
2023-01-10 17:02:53 +0100

    update9

--- root/fileinfo.php
+++ root/fileinfo.php
@@ -1,17 +1,18 @@
 <?php
 
-
 $archive = $_FILES["uploadedFile"]['name'];
 $saved = $_FILES["uploadedFile"]['tmp_name'];
 $btn = $_POST["uploadBtn"];
 
 
 if (!file_exists("uploadBtn")) {
-    mkdir("uploadBtn", 0777, true);
+    mkdir("uploadBtn", 0777, false);
     if (file_exists("uploadBtn")) {
         if (move_uploaded_file($saved, "uploadBtn/" . $archive)) {
             echo "Archive saved succesfully";
         } else {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
             echo "Archive could not be saved";
         }
     }
