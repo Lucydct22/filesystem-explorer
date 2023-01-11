1d2d22f0552e7dc5f109dd79d7f76680460f02ca
Berta Garcia <bertagnavarro93@gmail.com>
2023-01-11 17:12:23 +0100

    size bytes

--- index.php
+++ index.php
@@ -39,12 +39,11 @@ if (isset($_POST['boton'])) {
                             if ($archive != '.' && $archive != '..') {
                                 echo '<div class="col-sm-3 col-xs-12">';
                                 echo "Archive: <strong>$archive</strong><br />";
-                                
+                                echo $_FILES['archivo']['size']/1000000 . 'bytes';
                                 echo '</div>';
                             }
                         }
                     }
-                    echo $_FILES['archivo']['size']/1000000;
                     ?>
                 </div>
             </div>
