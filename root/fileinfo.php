aa980e85e2eb0df591de162ade801f8716bd50c3
lucydct <tenalucia0@gmail.com>
2023-01-11 11:08:16 +0100

    ´update12´

--- root/fileinfo.php
+++ root/fileinfo.php
@@ -59,8 +59,8 @@ if (!file_exists("uploadBtn")) {
 
 // Check the existence of file
 if (file_exists($file)) {
-    // Open the file for reading
-    $handle = fopen($file, "r") or die("ERROR: Cannot open the file.");
+    // Open the file for reading & writting
+    $handle = fopen($file, "r+") or die("ERROR: Cannot open the file.");
 
     // Read fixed number of bytes from the file
     $content = fread($handle, "20");
