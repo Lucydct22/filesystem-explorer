c8cdf01726e68f9d1a91323d457030fd3c60cb43
Berta Garcia <bertagnavarro93@gmail.com>
2023-01-16 09:58:46 +0100

    function navigate

--- folderAndFiles.php
+++ folderAndFiles.php
@@ -7,7 +7,7 @@ function folderAndFiles($dir)
     foreach ($content as $fileOrFolder) {
         if (is_dir($fileOrFolder)) {
             echo "
-                <div class='folder'>
+                <div class='folder' id='onclickCreateFolder' onclick='navigate()'>
                     <p href=$fileOrFolder>$fileOrFolder</p>
                 </div>
             ";
@@ -24,3 +24,6 @@ function folderAndFiles($dir)
 
     }
 }
+
+
+
--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const uploadArchive = document.getElementById('upload-btn');
 const uploadModal = document.getElementById('upload-modal');
 const createFolder = document.getElementById('createFolder-btn');
 const ppalContainer = document.getElementById('main-container');
+const onclickCreate = document.getElementById('onclickCreateFolder');
 
 
 uploadArchive.addEventListener("click", uploadFile)
 uploadModal.addEventListener("click", uploadFile)
 createFolder.addEventListener("click", createDirectory)
+onclickCreate.addEventListener("click", navigate)
 
 
 function uploadFile(e){
@@ -27,4 +29,23 @@ function createDirectory(e){
             ppalContainer.appendChild(newFolder)
         })
         .catch((err) => console.log("Request: ", err));    
+}
+
+function navigate(){
+  fetch('./folderAndFiles.php', {
+    method: "GET",
+  })
+  .then((response) => response.json())
+  .then ((data) => {
+    let newFile = document.createElement('p')
+    newFile.innerText = data.path
+    ppalContainer.appendChild(newFile)
+
+  })
+  .catch((err) => console.log("Request: ", err));  
+}
+
+
+location.reload(){
+return false;
 }
