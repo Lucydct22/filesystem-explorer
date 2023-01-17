cc7692eb35479aed2ec147f731326e59890075a0
lucydct <tenalucia0@gmail.com>
2023-01-17 09:26:21 +0100

    funtionfinalised3

--- delete.php
+++ delete.php
@@ -10,13 +10,13 @@ rrmdir($deleteFile);
 function rrmdir($dir)
 {
     if (is_dir($dir)) {
-        $objects = scandir($dir);
-        foreach ($objects as $object) {
-            if ($object != "." && $object != "..") {
-                if (filetype($dir . "/" . $object) == "dir") rrmdir($dir . "/" . $object);
-                else unlink($dir . "/" . $object);
-            }
-        }
+        // $objects = scandir($dir);
+        // foreach ($objects as $object) {
+        //     if ($object != "." && $object != "..") {
+        //         if (filetype($dir . "/" . $object) == "dir") rrmdir($dir . "/" . $object);
+        //         else unlink($dir . "/" . $object);
+        //     }
+        // }
         reset($objects);
         rmdir($dir);
         echo json_encode(["ok" => true]);
